975931f700e4f8866537ae4100122a50200b6877
Mohammedrisvanvm <risvanrishu0000@gmail.com>
2024-04-06 15:46:59 +0530

    added scrap controller

---
+++ controller/scrap-controller.js
@@ -0,0 +1,79 @@
+// Importing necessary modules
+import axios from "axios";
+import { load } from "cheerio";
+import fs from "fs";
+
+
+// Function to fetch HTML data from a given URL
+async function getHTML(url) {
+  const { data: html } = await axios.get(url);
+  return html;
+}
+
+// Controller function to scrape healthcare policy data
+export const scrape = (req, res) => {
+  try {
+    // Extracting URL from query parameters
+    const url = req.query.url;
+
+    // Object to store scraped data
+    const healthData = {};
+
+    // Fetching HTML data from the provided URL
+    getHTML(url).then(async (res) => {
+      // Parsing HTML using Cheerio
+      const $ = load(res);
+
+      // Iterating over each main section in the HTML
+      $("div.col-lg-12>main").each((i, element) => {
+        // Extracting title from each main section
+        const text = $(element).find("div h1").text().trim();
+
+        // Array to store medical policy documents
+        let MedicalPolicyDocuments = [];
+
+        // Iterating over each section within the main section
+        $(element)
+          .find("section")
+          .each((i, ul) => {
+            // Extracting children of each section
+            const children = $(ul).children();
+
+            // Iterating over each list item within the section
+            children.each((i, li) => {
+              // Extracting children of each list item
+              const children = $(li).children();
+
+              // Iterating over each link within the list item
+              children.each((i, a) => {
+                // Extracting link text and URL and adding it to MedicalPolicyDocuments array
+                MedicalPolicyDocuments.push({
+                  [$(a).text()]: `https://genhealth.ai${$(a)
+                    .find("a")
+                    .attr("href")}`,
+                });
+              });
+            });
+          });
+
+        // Storing title and medical policy documents in healthData object
+        healthData["title"] = text;
+        healthData["MedicalPolicyDocuments"] = MedicalPolicyDocuments;
+      });
+    
+
+      // Writing scraped data to a JSON file
+      fs.writeFile("scrapedData.json", JSON.stringify(healthData), (err) => {
+        if (err) throw err;
+
+        console.log("file succesfully saved!");
+      });
+    });
+
+    // Sending response indicating successful completion
+    res.status(201).json("done");
+  } catch (error) {
+    // Handling errors
+    res.status(500).json(error);
+  }
+};
